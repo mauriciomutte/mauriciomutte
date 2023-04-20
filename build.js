c6f638434775425d6704e57896f82cb066397f89
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-04-20 10:15:23 -0300

    feat: update `build.js` informations

--- build.js
+++ build.js
@@ -1,42 +1,42 @@
-'use strict';
+'use strict'
 
-const chalk = require('chalk');
-const boxen = require('boxen');
-const fs = require('fs');
-const path = require('path');
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
 
-const user = 'mauriciomutte';
+const user = 'mauriciomutte'
 
 const options = {
   padding: 2,
   margin: 2,
   borderStyle: 'round',
   align: 'center',
-};
+}
 
 const data = {
   name: chalk.blue.bold('Maurício Mutte'),
-  role: chalk.white('Full-stack Developer'),
+  role: chalk.white('Frontend Software Engineer'),
 
-  blog: chalk.green('https://mauriciomutte.com'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.blue(user),
+  blog: chalk.green('https://mauriciomutte.dev'),
   github: chalk.gray('https://github.com/') + chalk.blue(user),
+  twitter: chalk.gray('https://twitter.com/') + chalk.blue(user),
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue(user),
 
   labelBlog: chalk.green.bold('Blog:'),
-  labelTwitter: chalk.hex('#ffb86c').bold('  Twitter:'),
   labelGitHub: chalk.hex('#ffb86c').bold('  GitHub:'),
+  labelTwitter: chalk.hex('#ffb86c').bold('  Twitter:'),
   labelLinkedIn: chalk.hex('#ffb86c').bold('    LinkedIn:'),
-};
+}
 
-const newline = '\n';
+const newline = '\n'
 
-const heading = `${data.name}`;
-const role = `${data.role}`;
-const blog = `${data.labelBlog}  ${data.blog}`;
-const twitter = `${data.labelTwitter}  ${data.twitter}`;
-const github = `${data.labelGitHub}  ${data.github}`;
-const linkedin = `${data.labelLinkedIn}  ${data.linkedin}`;
+const heading = `${data.name}`
+const role = `${data.role}`
+const blog = `${data.labelBlog}  ${data.blog}`
+const twitter = `${data.labelTwitter}  ${data.twitter}`
+const github = `${data.labelGitHub}  ${data.github}`
+const linkedin = `${data.labelLinkedIn}  ${data.linkedin}`
 
 const output =
   heading +
@@ -47,13 +47,13 @@ const output =
   blog +
   newline +
   newline +
-  twitter +
-  newline +
   github +
   newline +
-  linkedin;
+  twitter +
+  newline +
+  linkedin
 
 fs.writeFileSync(
   path.join(__dirname, 'bin/output'),
   chalk.green(boxen(output, options))
-);
+)
